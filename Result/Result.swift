474e3c3f509f7d997a1a750e1072bebd6f23c631
John Gallagher <jgallagher@bignerdranch.com>
2014-09-12 12:56:08 -0400

    Initial commit

---
+++ Result/Result.swift
@@ -0,0 +1,39 @@
+//
+//  Result.swift
+//  Result
+//
+//  Created by John Gallagher on 9/12/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+public enum Result<T> {
+    case Failure(Error)
+
+    // TODO: Get rid of @autoclosure hack at some point after 6.1b1
+    case Success(@autoclosure () -> T)
+
+    public func map<U>(f: T -> U) -> Result<U> {
+        switch self {
+        case let .Failure(error): return .Failure(error)
+        case let .Success(value): return .Success(f(value()))
+        }
+    }
+
+    public func bind<U>(f: T -> Result<U>) -> Result<U> {
+        switch self {
+        case let .Failure(error): return .Failure(error)
+        case let .Success(value): return f(value())
+        }
+    }
+}
+
+extension Result: Printable {
+    public var description: String {
+        switch self {
+        case let .Failure(error): return "Result.Failure(\(error))"
+        case let .Success(value): return "Result.Success(\(value()))"
+        }
+    }
+}
