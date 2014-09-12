3534efedafffc3961084e31d50a497154cfef87e
John Gallagher <jgallagher@bignerdranch.com>
2014-09-12 13:05:53 -0400

    Add Mac target

---
+++ ResultMac/ResultMac.h
@@ -0,0 +1,19 @@
+//
+//  ResultMac.h
+//  ResultMac
+//
+//  Created by John Gallagher on 9/12/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ResultMac.
+FOUNDATION_EXPORT double ResultMacVersionNumber;
+
+//! Project version string for ResultMac.
+FOUNDATION_EXPORT const unsigned char ResultMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResultMac/PublicHeader.h>
+
+
